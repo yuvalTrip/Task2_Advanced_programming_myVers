480530a77f6e53bfd5e89a7fc39539866c64ce55
yuvalbs <yuvalbensimhon33@gmail.com>
2024-02-11 08:38:07 -0800

    prime counter add original code

--- primeCounter.c
+++ primeCounter.c
@@ -1,3 +1,36 @@
 //
 // Created by yuvalbs on 2/11/24.
 //
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+
+// Function to check if a number is prime
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    int total_counter = 0;
+
+    // Read numbers from stdin until end of file
+    while (scanf("%d", &num) != EOF) {
+        if (isPrime(num)) {
+            total_counter++;
+        }
+    }
+
+    printf("%d total primes.\n", total_counter);
+
+    return 0;
+}
